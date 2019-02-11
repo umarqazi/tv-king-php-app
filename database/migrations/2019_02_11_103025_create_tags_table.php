4c30634b8019e34b8328bd775177c4d15bc9879a
Umar1857 <umar.farooq@gems.techverx.com>
2019-02-11 16:48:34 +0500

    - Databse Migration - Spatie Permissions package. - Database Models.

--- database/migrations/2019_02_11_103025_create_tags_table.php
+++ database/migrations/2019_02_11_103025_create_tags_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('profile_image');
-            $table->rememberToken();
+            $table->string('name');
+
+            /*Challenge ID as Foreign Key in Tags Table*/
+            $table->unsignedInteger('challenge_id')->nullable();
+            $table->foreign('challenge_id')->references('id')->on('challenges')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
@@ -32,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tags');
     }
 }
