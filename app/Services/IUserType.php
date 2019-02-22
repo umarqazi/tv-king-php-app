8e46ad7548ca747bca7a578c7f13d15bc4bd581e
Fahad Shehzad <fahad.shehzad@gems.techverx.com>
2019-02-22 18:28:51 +0500

    Added UserType Interface

---
+++ app/Services/IUserType.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fahad
+ * Date: 22/02/2019
+ * Time: 6:28 PM
+ */
+
+namespace App\Services;
+
+
+interface IUserType
+{
+    const ADMIN = 1;
+    const CUSTOMER = 2;
+    const BRAND = 3;
+}
