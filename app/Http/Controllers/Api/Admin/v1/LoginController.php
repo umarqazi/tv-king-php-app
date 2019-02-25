375b44e33e5f933da38f73859ebc3a83c59ff800
Fahad Shehzad <fahad.shehzad@gems.techverx.com>
2019-02-25 11:30:11 +0500

    added seperate LoginControllers for all API's

---
+++ app/Http/Controllers/Api/Admin/v1/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fahad
+ * Date: 25/02/2019
+ * Time: 11:29 AM
+ */
+
+namespace App\Http\Controllers\Api\Admin\v1;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Class LoginController
+ * @package App\Http\Controllers\Api\Admin\v1
+ *
+ */
+class LoginController extends Controller
+{
+    
+
+}
---
+++ app/Http/Controllers/Api/Brand/v1/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fahad
+ * Date: 25/02/2019
+ * Time: 11:28 AM
+ */
+
+namespace App\Http\Controllers\Api\Brand\v1;
+
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Class LoginController
+ * @package App\Http\Controllers\Api\Brand\v1
+ *
+ */
+class LoginController extends Controller
+{
+
+}
